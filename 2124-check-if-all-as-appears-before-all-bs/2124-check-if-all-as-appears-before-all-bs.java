b89815d78bb479bb98ceccd8bbf8e1e773a491c7
aviralzzz <aviralsingh9c@gmail.com>
2026-08-05 01:04:39 +0530

    Time: 0 ms (100%), Space: 43 MB (42.14%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -1,13 +1,6 @@
 class Solution {
     public boolean checkString(String s) {
-        int a=s.lastIndexOf('a');
-        int b=s.indexOf('b');
-        if (a == -1 || b == -1) {
-            return true;}
-        
-        if(b>a)
-        return true;
-        else return false;
+        return(!s.contains("ba"));
         
     }
 }
